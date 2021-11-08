2082fceb1d9028d8d34032ef342f8bc83e5e5acc
Ludovic T <thuard.ludovic@gmail.com>
2021-11-08 15:20:40 +0100

    rework homepage.js

--- front/js/homepage.js
+++ front/js/homepage.js
@@ -1,64 +1,24 @@
-const $productsWrapper = document.querySelector('.productContainer');
+const $itemsWrapper = document.querySelector('.items');
 
-const ITEMS_PER_PAGE = 8
+const ITEMS_PER_PAGE = 8;
 
-//On va chercher les infos
-
-const retrieveProductsData = () => fetch('/api/product.json')
-    .then(res => res.json())
+const retrieveProductsData =() => fetch('/api/products.json')
+    .then(response => response.json())
     .then(data => data.products)
-    .catch(err => console.log ("Erreur", err))
+    .catch(error => console.log(error));
 
-//On va chercher de quoi remplir la carte
-
-const createProductCardImage = products => {
-    const $productImage = document.createElement('img')
-    $productImage.classList.add('item__img')
-    $productImage.setAttribute('src', `../back/images/${products.imageURL}`)
-    $productImage.setAttribute('alt', `${products.name}`)
-
-    return $productImage
-}
-
-const createProductCardInfos = products =>{
-    const $productInfo = document.createElement('div')
-    $productInfo.classList.add('.productContainer')
-
-    const $productInfoTitle = document.createElement('h3')
-    $productInfoTitle.textContent = `${products.name}`
-
-    const $productInfoDescription = document.createElement('p')
-    $productInfoDescription.textContent = `${products.description}`
-
-    const $productButton = document.createElement ('a')
-    $productButton.classList.add('products-button')
-    $productButton.setAttribute('href', `../front/html/product.html?productID=${product.name}`)
-
-    $productInfo.appendChild($productInfoTitle)
-    $productInfo.appendChild($productInfoDescription)
-    $productInfo.appendChild($productButton)
-
-    return $productInfo
-}
-
-//On crée la carte
-
-const createProductCard = products => {
-    const $productCard = document.createElement('div')
-    $productCard.classList.add('item')
-    const $productImage = createProductCardImage(products)
-    const $productInfo = createProductCardInfos(products)
-
-    $productCard.appendChild($productImage)
-    $productCard.appendChild($productInfo)
-
-    return $productCard
-}
-
-//Affichage de la carte
-
-const displayProducts = products => {
-    products.forEach(products => {
-        const $productCard = createProductCard(products)
-        $productsWrapper.appendChild($productCard)
-    })}
+const renderProducts = (products) => {
+    const $items = products.map(product => {
+        const $item = document.createElement('a');
+        $item.href = `./product?${product.id}`;
+        $item.innerHTML = `
+            <article>
+                <img src=".../${product.image}" alt="${product.name}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+            </article>
+        `;
+        return $item;
+    });
+    $itemsWrapper.append($items);
+};
