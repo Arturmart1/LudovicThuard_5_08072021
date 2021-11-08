5a743bb8ec9eefa4c41fca7d3f738b9608a46374
Ludovic T <thuard.ludovic@gmail.com>
2021-11-08 16:35:35 +0100

    rework homapage.js

--- front/js/homepage.js
+++ front/js/homepage.js
@@ -1,24 +1,44 @@
-const $itemsWrapper = document.querySelector('.items');
+//Récuperation des produits
+async function getProducts() {
+    let products = await fetch("http://localhost:3000/api/products")
+    return await products.json();
+}
 
-const ITEMS_PER_PAGE = 8;
+//Remplissage du DOM
+async function fillProducts(){
+    let catalogue = await getProducts()
+    .then(function(APIresult){
+        const products = APIresult;
+        console.table(products);
+        for (let product in products){
 
-const retrieveProductsData =() => fetch('/api/products.json')
-    .then(response => response.json())
-    .then(data => data.products)
-    .catch(error => console.log(error));
+            //Création de l'élément <a>
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.setAttribute("href", "product.html?id=${APIresult[article]._id}");
 
-const renderProducts = (products) => {
-    const $items = products.map(product => {
-        const $item = document.createElement('a');
-        $item.href = `./product?${product.id}`;
-        $item.innerHTML = `
-            <article>
-                <img src=".../${product.image}" alt="${product.name}">
-                <h3 class="productName">${product.name}</h3>
-                <p class="productDescription">${product.description}</p>
-            </article>
-        `;
-        return $item;
-    });
-    $itemsWrapper.append($items);
-};
+            //Création de l'élément <article>
+            let productCard = document.createElement("article");
+            productCard.appendChild(productLink);
+
+            //Insertion de l'image
+            let productImage = document.createElement("img");
+            productCard.appendChild(productImage);
+            productImage.setAttribute("src", "${APIresult[.imageURL]");
+            productImage.setAttribute("alt", "${APIresult[.name]");
+
+            //Insertion du titre
+            let productTitle = document.createElement("h3");
+            productCard.appendChild(productTitle);
+            productTitle.classList.add("productName");
+            productTitle.innerHTML = "${APIresult[.name]";
+
+            //Insertion de la description
+            let productDescription = document.createElement("p");
+            productCard.appendChild(productDescription);
+            productDescription.classList.add("productDescription");
+            productDescription.innerHTML = "${APIresult[.description]";
+        }
+    })
+}
+fillProducts();
