b565b00517a62867c177c482ec5c7707527adea8
Ludovic T <thuard.ludovic@gmail.com>
2021-11-09 08:48:46 +0100

    rework script.js rework script for index.html ISSUE : Nothing to show

--- front/js/index.js
+++ front/js/index.js
@@ -1,43 +1,43 @@
-//Récuperation des produits
+//Récuperation des articles
 async function getProducts() {
-    let products = await fetch("http://localhost:3000/api/products")
-    return await products.json();
+    let productsData = await fetch ("http://localhost:3000/api/products")
+
+    return productsData.json();
 }
 
-//Remplissage du DOM
-async function productsList(){
-    let catalogue = await getProducts()
-    .then(function(APIresult){
-        const products = APIresult;
+function productsCards(){
+    let productsList = getProducts()
+    .then(function (resApi){
+        const products = resApi;
         for (let product in products){
 
             //Création de l'élément <a>
             let productLink = document.createElement("a");
             document.querySelector(".items").appendChild(productLink);
-            productLink.setAttribute("href", "product.html?id=${APIresult[article]._id}");
+            productLink.setAttribute( `product.html?id=${resApi[product]._id}` );
 
             //Création de l'élément <article>
             let productCard = document.createElement("article");
-            productCard.appendChild(productLink);
+            productLink.appendChild(productCard);
 
             //Insertion de l'image
             let productImage = document.createElement("img");
+            productImage.setAttribute("src", resApi[product].imageUrl);
+            productImage.setAttribute("alt", resApi[product].altTxt);
             productCard.appendChild(productImage);
-            productImage.setAttribute("src", APIresult[product].imageUrl);
-            productImage.setAttribute("alt", products[product].altTxt);
 
             //Insertion du titre
             let productTitle = document.createElement("h3");
-            productCard.appendChild(productTitle);
             productTitle.classList.add("productName");
-            productTitle.innerHTML = "${APIresult[.name]";
+            productTitle.innerHTML = resApi[product].name;
+            productCard.appendChild(productTitle);
 
             //Insertion de la description
             let productDescription = document.createElement("p");
-            productCard.appendChild(productDescription);
             productDescription.classList.add("productDescription");
-            productDescription.innerHTML = "${APIresult[.description]";
+            productDescription.innerHTML = resApi[product].description;
+            productCard.appendChild(productDescription);
         }
     })
 }
-productsList();
+productsCards();
